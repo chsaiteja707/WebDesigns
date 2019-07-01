87f1f8626c33f078a169bb7ec94fe098edc1816b
Sai Teja Teja Chelluboina <40346354+chsaiteja707@users.noreply.github.com>
2019-07-01 07:19:26 +0530

    Add files via upload

---
+++ checklist.js
@@ -0,0 +1,40 @@
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");	
+});
+//without using .completed class in css file
+/*
+$("li").click(function(){
+	console.log($(this).css("color"));
+	if($(this).css("color")==="rgb(255, 0, 0)"){
+		$(this).css("color","black");
+		$(this).css("textDecoration","none");
+	}
+	else{
+		$(this).css({
+		color:"red",
+		textDecoration:"line-through"
+		});
+})
+	*/	
+
+$("span").click(function(event){
+	event.stopPropagation();
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+});
+
+$("input[type='text']").keypress(function(event)
+{
+	if(event.which===13)
+	{
+		var k=$(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span>"+k+"</li>");
+	}
+
+})
+
+$(".fa-plus").click(function(){
+	$("input[type='text").fadeToggle();
+})
